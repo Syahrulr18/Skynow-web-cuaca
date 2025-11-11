3f35ddce491dadfbfccd902f22e1b565e7aa948d
Syahrulr18 <muhsyahrulramadhan2006@gmail.com>
2025-11-11 10:27:49 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center align-middle'>
+      <p>test</p>
+    </div>
+  )
+}
+
+export default App
